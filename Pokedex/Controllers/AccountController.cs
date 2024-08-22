87268a0ef2928c3298b9ccbdb3ff63c27c8b87d8
lucas-catto <lucascattosilva2018@gmail.com>
2024-08-21 22:22:49 -0300

    Codificar a página de login.

--- Pokedex/Controllers/AccountController.cs
+++ Pokedex/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 
+using System.Net.Mail;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.ViewModels;
 
@@ -7,10 +10,17 @@ namespace Pokedex.Controllers;
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
+    private readonly ILogger<AccountController> _signInManager;
+    private readonly ILogger<AccountController> _userManager;
 
-    public AccountController(ILogger<AccountController> logger)
-    {
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager
+    ) {
         _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     [HttpGet]
@@ -22,6 +32,76 @@ public class AccountController : Controller
         };
 
         return View(loginVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string username = login.Email;
+
+            if (isValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+
+                if (user != null)
+                {
+                    userName = user.username;
+                }
+
+                var result = await _signInManager.passwordSignInAsync(
+                    username,
+                    login.Senha,
+                    login.Lembrar,
+                    lockoutOnFaliure: true
+                );
+
+                if (result.isSucced)
+                {
+                    _logger.LogInformation($"Usuário {login.Email} acessou o sistema!");
+                    return LocalRedirect(login.UrlRetorno);
+                }
+
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning($"Usuário {login.Email} foi bloqueado!");
+                    return RedirectToAction("Lockout");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Credênciais inválidas!");
+        }
+
+        return View(login);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logout.");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Lockout()
+    {
+        return View();
+    }
+
+    private bool isValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail= new(email);
+            return true;
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
